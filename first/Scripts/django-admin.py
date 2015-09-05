ca7f978cd746fe0c8f9ad66c74eb73ef1173dfb1
aliomid730 <ali.omid730@gmail.com>
2015-09-05 08:30:49 +0430

    my blog

---
+++ first/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\aliomid730\Desktop\Django Projects\First\first\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
